94e433a195ed4a3116a2576503ac257db8145f43
36280-source <36280@saard.ac.th>
2026-06-28 13:56:37 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,12 @@
 // Import Firebase SDK
 const firebaseConfig = {
-
-  apiKey: "ใส่ของคุณ",
-
-  authDomain: "ใส่ของคุณ",
-
-  projectId: "ใส่ของคุณ",
-
-  storageBucket: "ใส่ของคุณ",
-
-  messagingSenderId: "ใส่ของคุณ",
-
-  appId: "ใส่ของคุณ"
-
+  apiKey: "AIzaSyCvWAznYtpjtRsXm4g9FuW4ZFr4d1MfJF4",
+  authDomain: "yakisoba-order.firebaseapp.com",
+  projectId: "yakisoba-order",
+  storageBucket: "yakisoba-order.firebasestorage.app",
+  messagingSenderId: "902273705180",
+  appId: "1:902273705180:web:27451fd8f173a1e93ec019",
+  measurementId: "G-3GHWHWRBRF"
 };
 
 // เริ่มใช้งาน Firebase
